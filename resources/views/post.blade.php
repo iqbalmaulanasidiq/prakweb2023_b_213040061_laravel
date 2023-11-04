3eadc6fe13863bbc83b6174329d9ab3283d0bd04
iqbalmaulanasidiq <iqbal.213040061@mail.unpas.ac.id>
2023-11-04 18:50:08 +0700

    #8

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,6 +2,8 @@
 @section('container')
 <article>
     <h2>{{ $post->title }}</h2>
+    {{-- <p>By. Iqbal Maulana Sidiq {{ $post->category->name }}</p> --}}
+    <p>By. Iqbal Maulana Sidiq in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
    {!! $post->body !!}
 </article>
 <a href="/posts">Back to posts</a>
