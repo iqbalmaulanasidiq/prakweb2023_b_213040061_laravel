b972a7b1336f8ef65e1aab61a32430d1646ce732
iqbalmaulanasidiq <iqbal.213040061@mail.unpas.ac.id>
2023-11-19 13:32:37 +0700

    14

---
+++ resources/views/register/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.main')
+
+@section('container')
+
+<div class="row justify-content-center">
+    <div class="col-lg-5">
+        <main class="form-registration w-100 m-auto">
+            <h1 class="h3 mb-3 fw-normal text-center">registration Form</h1>
+            <form>
+                <div class="form-floating">
+                    <input type="text" name="name" class="form-control rounded-top" id="name" placeholder="name">
+                    <label for="name">Name</label>
+                </div>
+                <div class="form-floating">
+                    <input type="text" name="username" class="form-control" id="username" placeholder="username">
+                    <label for="username">username</label>
+                </div>
+              <div class="form-floating">
+                <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com">
+                <label for="email">Email address</label>
+              </div>
+              <div class="form-floating">
+                <input type="password" name="password" class="form-control rounded-bottom" id="password" placeholder="Password">
+                <label for="password">Password</label>
+              </div>
+              <button class="btn btn-primary w-100 py-2 mt-5" type="submit">Register</button>
+            </form>
+            <small class="d-block text-center mt-3">All ready registered? <a href="/login" class="text-decoration-none">Login Now!</a></small>
+          </main>
+    </div>
+</div>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Models\Post;
+// use App\Models\Post;
+// use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-use App\Models\Category;
-use App\Models\User;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 
 /*
@@ -64,3 +66,7 @@ Route::get('/categories', function(){
 //         'posts' => $author->posts->load('category', 'author'),
 //     ]);
 // });
+
+//login
+Route::get('/login' , [LoginController::class, 'index'])->name('login');
+Route::get('/register' , [RegisterController::class, 'index'])->name('register');
