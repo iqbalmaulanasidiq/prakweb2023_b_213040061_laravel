c3be38109a093f20cdded9108e17f4094405c6ab
iqbalmaulanasidiq <iqbal.213040061@mail.unpas.ac.id>
2023-11-26 11:10:51 +0700

    23

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use App\Models\Category;
 use App\Models\User;
 use App\Http\Controllers\DashboardPostController;
 
+use App\Http\Controllers\AdminCategoryController;
+
 
 
 /*
@@ -87,3 +89,4 @@ Route::get('/dashboard', function () {
 Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
 
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
